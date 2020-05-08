bfc181d9dacc0a95ceb8f3dd040d6bf1c0a52992
fx-masterblock <a.ruizg1450@gmail.com>
2020-05-08 14:31:03 -0500

    inizialite.py connected to account

--- initialize.py
+++ initialize.py
@@ -9,37 +9,25 @@ print ("metatrader5 package version",mt5.__version__)
 print ("autors of the bot: Machine block and R6Doc")
 print ("bot version 1.0")
 
-# establish MetaTrader 5 connection to a specified trading account
+# establish connection to the MetaTrader 5 terminal
+if not mt5.initialize(login=27865035, server="MetaQuotesDemo",password="vlnp3nkt"):
+    print("initialize() failed, error code =",mt5.last_error())
+    
 
-if mt5.initialize(login=27865035, server="MetaQuotes-Demo",password="vlnp3nkt"):
-    print("initialize failed error code =",mt5.last_error())
-   
-
-# display data on metatrader5
+# display data on MetaTrader 5 version
 print(mt5.version())
+# connect to the trade account without specifying a password and a server
+account=27865035
+authorized=mt5.login(account,password="vlnp3nkt")  # the terminal database password is applied if connection data is set to be remembered
+if authorized:
+    print("connected to account #{}".format(account))
+else:
+    print("failed to connect at account #{}, error code: {}".format(account, mt5.last_error()))
  
-# display info on the terminal settings and status
-if terminal_info != None:
+# now connect to another trading account specifying the password
 
-   print(terminal_info)
-terminal_info=mt5.terminal_info()
-if terminal_info!=None:
-    # display the terminal data 'as is'
-    print(terminal_info)
-    # display data in the form of a list
-    print("Show terminal_info()._asdict():")
-    terminal_info_dict = mt5.terminal_info()._asdict()
-    for prop in terminal_info_dict:
-        print("  {}={}".format(prop, terminal_info_dict[prop]))
-    print()
-   # convert the dictionary into DataFrame and print
-    df=pd.DataFrame(list(terminal_info_dict.items()),columns=['property','value'])
-    print("terminal_info() as dataframe:")
-    print(df)
  
 # shut down connection to the MetaTrader 5 terminal
 mt5.shutdown()
-
-
-
-
+ 
+        

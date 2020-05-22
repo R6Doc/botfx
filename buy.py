7dadd85ccd48428b338103ba60d5d865ec9bccf0
fx-masterblock <a.ruizg1450@gmail.com>
2020-05-21 22:43:19 -0500

    jajsja

--- buy.py
+++ buy.py
@@ -5,5 +5,26 @@ import pandas as pd
 from ordertick import rates
 
 def buy1():
-    print("hola mundo")
+ while True: 
+     if rates > 30:
+            symbol=("EURUSD")
+     
+            lot = 0.1
+            point = mt5.symbol_info(symbol).point
+            price = mt5.symbol_info_tick(symbol).ask
+            deviation = 20
+            request = {
+                "action": mt5.TRADE_ACTION_DEAL,
+                "symbol": symbol,
+                "volume": lot,
+                "type": mt5.ORDER_TYPE_BUY,
+                "price": price,
+                "tp": price + 80 * point,
+                "deviation": deviation,
+                "magic": 234000,
+                "comment": "python script open",
+                "type_time": mt5.ORDER_TIME_GTC,
+                "type_filling": mt5.ORDER_FILLING_RETURN,
+            }
+buy1()
 
